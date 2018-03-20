da5d9ad7efee25d6bbdfc4e6ed030d15181a4da6
Micah Smith <micahjsmith@gmail.com>
2018-03-20 10:50:37 -0400

    Add null_counts pipe

--- pandas_dry/pipes/pipes.py
+++ pandas_dry/pipes/pipes.py
@@ -17,7 +17,7 @@ def flatten_multiindex(df, sep, axis=1):
     df.set_axis(axis, df._get_axis(axis).map(lambda x: flatten_strings(x, sep=sep)))
     return df
 
-def add_level(df, value, axis=1, name=None):
+def add_level(df, value, name=None, axis=1):
     df = df.copy()
     axis_ = df._get_axis(axis)
     names = axis_.names
@@ -32,3 +32,13 @@ def drop_level(df, level, axis=1):
     df = df.copy()
     df.set_axis(axis, df._get_axis(axis).droplevel(level))
     return df
+
+# TODO Make compatible for both Series and DataFrame
+# TODO Accept different null value (such as 'NA' instead of np.nan)
+def null_counts(ser):
+    n = len(ser)
+    n_null = ser.isnull().sum()
+    n_notnull = n - n_null
+    return pd.Series(data=[n_notnull, n_null],
+                     index=['Not Null', 'Null'],
+                     name=ser.name)
